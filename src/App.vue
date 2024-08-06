4b8af2f690776d7e8e09272f3f3f611b4fec9269
Mohamed Bankolé <medbankole97@gmail.com>
2024-08-06 13:00:14 +0000

    second commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld :name="name" :age="age"/>
+
+  <input type="text" v-model="name">
+  <input type="number" v-model="age">
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import { ref } from 'vue';
+
+const name = ref('NordCoders');
+const age = ref(30);
+</script>
+
+<style scoped>
+/* Votre style ici */
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <h1> Hello World!</h1>
+  <p>{{props.name}} {{props.age}}</p>
+  
+</template>
+
+<script setup>
+const props = defineProps(['name', 'age']);
+
+</script>
